19a1513a988fdc556b5050ba973876caab1cbfa3
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-03-22 12:46:08 +0900

    fix gitignore

--- packages/use-shader-fx/node_modules/@react-three/fiber/dist/declarations/src/native/events.d.ts
+++
@@ -1,5 +0,0 @@
-import { UseBoundStore } from 'zustand';
-import { RootState } from '../core/store';
-import { EventManager } from '../core/events';
-/** Default R3F event manager for react-native */
-export declare function createTouchEvents(store: UseBoundStore<RootState>): EventManager<HTMLElement>;
