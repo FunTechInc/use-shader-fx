606db23e5d09f889e872a8eb70d6ef6c3fae0e11
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-05-29 21:41:44 +0900

    v1.1.35

--- packages/use-shader-fx/src/utils/useResolution.ts
+++ packages/use-shader-fx/src/utils/useResolution.ts
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { useMemo } from "react";
 import { Size } from "@react-three/fiber";
 
-/**0
+/**
  * @params dpr if dpr is set, it returns the resolution which is size multiplied by dpr.
  */
 export const useResolution = (size: Size, dpr: number | false = false) => {
