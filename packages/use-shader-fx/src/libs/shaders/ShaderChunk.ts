f4b1a82f1814cf96a3a3c8b93101ef522c3d124d
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-06-01 12:23:59 +0900

    add hsv to chunk

--- packages/use-shader-fx/src/libs/shaders/ShaderChunk.ts
+++ packages/use-shader-fx/src/libs/shaders/ShaderChunk.ts
@@ -4,6 +4,8 @@ import coverTexture from "./shaderChunk/coverTexture.glsl";
 import fxBlending from "./shaderChunk/fxBlending.glsl";
 import planeVertex from "./shaderChunk/planeVertex.glsl";
 import defaultVertex from "./shaderChunk/defaultVertex.glsl";
+import hsv2rgb from "./shaderChunk/hsv2rgb.glsl";
+import rgb2hsv from "./shaderChunk/rgb2hsv.glsl";
 
 export type ShaderChunkTypes =
    | "wobble3D"
@@ -11,7 +13,9 @@ export type ShaderChunkTypes =
    | "coverTexture"
    | "fxBlending"
    | "planeVertex"
-   | "defaultVertex";
+   | "defaultVertex"
+   | "hsv2rgb"
+   | "rgb2hsv";
 
 export const ShaderChunk: { [K in ShaderChunkTypes]: string } = Object.freeze({
    wobble3D,
@@ -20,4 +24,6 @@ export const ShaderChunk: { [K in ShaderChunkTypes]: string } = Object.freeze({
    fxBlending,
    planeVertex,
    defaultVertex,
+   hsv2rgb,
+   rgb2hsv,
 });
--- packages/use-shader-fx/types/libs/shaders/ShaderChunk.d.ts
+++ packages/use-shader-fx/types/libs/shaders/ShaderChunk.d.ts
@@ -1,4 +1,4 @@
-export type ShaderChunkTypes = "wobble3D" | "snoise" | "coverTexture" | "fxBlending" | "planeVertex" | "defaultVertex";
+export type ShaderChunkTypes = "wobble3D" | "snoise" | "coverTexture" | "fxBlending" | "planeVertex" | "defaultVertex" | "hsv2rgb" | "rgb2hsv";
 export declare const ShaderChunk: {
     [K in ShaderChunkTypes]: string;
 };
