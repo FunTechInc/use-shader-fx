72543de8d5b37b6ef1568e671503c611c9043ea2
roku-ft <r.jokura@funtech.inc>
2025-03-15 14:04:53 +0900

    update gui

--- app/nodeui/page.tsx
+++ app/nodeui/page.tsx
@@ -13,9 +13,16 @@ import {
 } from '@xyflow/react';
 
 
+// Nodes
+import NoiseNode from './nodes/noise';
+
+const nodeTypes = { textUpdater: NoiseNode };
+
+
 const initialNodes = [
    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+   { id: '3', position: { x: 0, y: 200 }, type: 'textUpdater', data: { label: '3' } },
  ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
@@ -31,6 +38,7 @@ export default function Page() {
             edges={edges}
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
+            nodeTypes={nodeTypes}
             fitView
             // onConnect={onConnect}
          />
