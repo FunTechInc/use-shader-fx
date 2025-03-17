05b12f1498aaada1c056b5321bed17220a1b1c75
roku-ft <r.jokura@funtech.inc>
2025-03-17 22:37:43 +0900

    feat: add NoiseNode parameters and GUI integration for basic effects

--- app/nodeui/nodes/noise/index.tsx
+++ app/nodeui/nodes/noise/index.tsx
@@ -5,29 +5,185 @@ import s from './style.module.scss';
 import { GUI } from 'lil-gui';
 import { useGUI } from '@/utils/useGUI';
 
+import { NoiseValues } from '@/packages/use-shader-fx/src';
+// import { BASICFX_VALUES } from '@/packages/use-shader-fx/src';
+import * as THREE from 'three';
  
-const handleStyle = { left: 10 };
- 
-function NoiseNode({ data }) {
-  
-  const parametersEl = useRef<HTMLDivElement>(null);  
+export const BASICFX_VALUES = {
+  mixSrc: {
+    /*===============================================
+      mixSrc
+    ===============================================*/
+    mixSrc: { value: false },
+    mixSrc_src: { value: new THREE.Texture() },
+    mixSrc_fit: { value: 0 },
+    mixSrc_fitScale: { value: new THREE.Vector2(1, 1) },
 
+    // uv
+    mixSrc_uv: { value: false },
+    mixSrc_uv_ch: { value: 0 },
+    mixSrc_uv_factor: { value: 0 },
+    mixSrc_uv_offset: { value: new THREE.Vector2(0, 0) },
+    mixSrc_uv_radius: { value: 0.5 },
+    mixSrc_uv_position: { value: new THREE.Vector2(0.5, 0.5) },
+    mixSrc_uv_range: { value: new THREE.Vector2(0.0, 1.0) },
+    mixSrc_uv_mixMap: { value: false },
+    mixSrc_uv_mixMap_ch: { value: 0 },
+
+    // color
+    mixSrc_color: { value: false },
+    mixSrc_color_factor: { value: 0 },
+    mixSrc_color_radius: { value: 0.5 },
+    mixSrc_color_position: { value: new THREE.Vector2(0.5, 0.5) },
+    mixSrc_color_range: { value: new THREE.Vector2(0.0, 1.0) },
+    mixSrc_color_mixMap: { value: false },
+    mixSrc_color_mixMap_ch: { value: 0 },
+
+    // alpha
+    mixSrc_alpha: { value: false },
+    mixSrc_alpha_factor: { value: 0 },
+    mixSrc_alpha_radius: { value: 0.5 },
+    mixSrc_alpha_position: { value: new THREE.Vector2(0.5, 0.5) },
+    mixSrc_alpha_range: { value: new THREE.Vector2(0.0, 1.0) },
+    mixSrc_alpha_mixMap: { value: false },
+    mixSrc_alpha_mixMap_ch: { value: 0 },
+  },
+  mixDst: {
+    /*===============================================
+      mixDst
+    ===============================================*/
+    mixDst: { value: false },
+    mixDst_src: { value: new THREE.Texture() },
+    mixDst_fit: { value: 0 },
+    mixDst_fitScale: { value: new THREE.Vector2(1, 1) },
+
+    // uv
+    mixDst_uv: { value: false },
+    mixDst_uv_ch: { value: 0 },
+    mixDst_uv_factor: { value: 0 },
+    mixDst_uv_offset: { value: new THREE.Vector2(0, 0) },
+    mixDst_uv_radius: { value: 0.5 },
+    mixDst_uv_position: { value: new THREE.Vector2(0.5, 0.5) },
+    mixDst_uv_range: { value: new THREE.Vector2(0.0, 1.0) },
+    mixDst_uv_mixMap: { value: false },
+    mixDst_uv_mixMap_ch: { value: 0 },
+
+    // color
+    mixDst_color: { value: false },
+    mixDst_color_factor: { value: 0 },
+    mixDst_color_radius: { value: 0.5 },
+    mixDst_color_position: { value: new THREE.Vector2(0.5, 0.5) },
+    mixDst_color_range: { value: new THREE.Vector2(0.0, 1.0) },
+    mixDst_color_mixMap: { value: false },
+    mixDst_color_mixMap_ch: { value: 0 },
+
+    // alpha
+    mixDst_alpha: { value: false },
+    mixDst_alpha_factor: { value: 0 },
+    mixDst_alpha_radius: { value: 0.5 },
+    mixDst_alpha_position: { value: new THREE.Vector2(0.5, 0.5) },
+    mixDst_alpha_range: { value: new THREE.Vector2(0.0, 1.0) },
+    mixDst_alpha_mixMap: { value: false },
+    mixDst_alpha_mixMap_ch: { value: 0 },
+  },
+  adjustments: {
+    /*===============================================
+      adjustments
+    ===============================================*/
+    // levels
+    levels: { value: false },
+    levels_shadows: { value: new THREE.Vector4(0, 0, 0, 0) },
+    levels_midtones: { value: new THREE.Vector4(1, 1, 1, 1) },
+    levels_highlights: { value: new THREE.Vector4(1, 1, 1, 1) },
+    levels_outputMin: { value: new THREE.Vector4(0, 0, 0, 0) },
+    levels_outputMax: { value: new THREE.Vector4(1, 1, 1, 1) },
+
+    // contrast
+    contrast: { value: false },
+    contrast_factor: { value: new THREE.Vector4(1, 1, 1, 1) },
+
+    // colorBalance
+    colorBalance: { value: false },
+    colorBalance_factor: { value: new THREE.Vector3(1, 1, 1) },
+
+    // hsv
+    hsv: { value: false },
+    hsv_hueShift: { value: 0 },
+    hsv_saturation: { value: 1 },
+    hsv_brightness: { value: 1 },
+
+    // posterize
+    posterize: { value: false },
+    posterize_levels: { value: new THREE.Vector4(0, 0, 0, 0) },
+
+    // grayscale
+    grayscale: { value: false },
+    grayscale_weight: { value: new THREE.Vector3(0, 0, 0) },
+    grayscale_duotone: { value: false },
+    grayscale_duotone_color0: { value: new THREE.Color(0x000000) },
+    grayscale_duotone_color1: { value: new THREE.Color(0xffffff) },
+    grayscale_threshold: { value: -1 },
+  }
+};
+
+
+export const NoiseInitParams = {   
+   tick: 0,   
+   scale: 0.004,   
+   timeStrength: 0.3,   
+   noiseOctaves: 1,   
+   fbmOctaves: 2,   
+   warpOctaves: 2,   
+   warpDirection: new THREE.Vector2(2, 2),  
+   warpStrength: 8,   
+   timeOffset: 0,  
+   ...BASICFX_VALUES
+};
+ 
+function NoiseNode({ data: {params} }: {data: {
+  params: any
+}}) {        
+  const parametersEl = useRef<HTMLDivElement>(null);  
+  
     const setupGUI = useCallback(
         (gui: GUI) => {
-
+            const noiseParams = gui.addFolder('Noise Parameters');
+            noiseParams.add(params, 'scale', 0, 10, 0.001);
+            noiseParams.add(params, 'timeStrength', 0, 10, 0.01);
+            noiseParams.add(params, 'noiseOctaves', 1, 10, 1);
+            noiseParams.add(params, 'fbmOctaves', 1, 10, 1);
+            noiseParams.add(params, 'warpOctaves', 1, 10, 1);
+            noiseParams.add(params.warpDirection, 'x', 1, 10, 0.1);
+            noiseParams.add(params.warpDirection, 'y', 1, 10, 0.1);
+            noiseParams.add(params, 'warpStrength', 1, 50, 0.1);   
+            
+            const basicFxFolder = gui.addFolder('Basic FX Values');
+              
+            // mixSrc フォルダーの追加 繋がっているか繋がっていないかで表示を変える
+            const mixSrcFolder = basicFxFolder.addFolder('mixSrc');          
+            // mixDst フォルダーの追加　繋がっているか繋がっていないかで表示を変える
+            const mixDstFolder = basicFxFolder.addFolder('mixDst');                        
+            // adjustments フォルダーの追加
+            const adjustmentsFolder = basicFxFolder.addFolder('adjustments');
+              
         },
-        [data,parametersEl]
+        [params,parametersEl]
     );
-    const updateBasicFxGUI = useGUI(setupGUI, "Noise", parametersEl);
+    const updateBasicFxGUI = useGUI(setupGUI, "Parameters", parametersEl);
  
   return (
     <div className={s.node}>
       <Handle type="target" position={Position.Left} />
-      <div className={s.nodeHead}>
+      <div className={`${s.nodeHead} draghandle`}>
         Noise
       </div>
-      <div className={s.nodeBody} ref={parametersEl}>
-        aaa
+      <div className={s.nodeBody}>
+        <div className={`${s.nodeSide} draghandle`}>
+          
+        </div>
+        <div className={s.nodeMain} ref={parametersEl}>
+
+        </div>
       </div>
       
       <Handle type="source" position={Position.Right} id="a" />
--- app/nodeui/page.tsx
+++ app/nodeui/page.tsx
@@ -3,31 +3,47 @@ import s from './index.module.scss';
 import '@xyflow/react/dist/style.css';
 
 import React,{
-   useCallback
+   useCallback,
+   useEffect
 } from 'react';
 import { 
    ReactFlow,
    useNodesState,
    useEdgesState,
-   addEdge,  
+   addEdge,
+   Node,  
 } from '@xyflow/react';
 
 
 // Nodes
-import NoiseNode from './nodes/noise'
+// Noise
+import NoiseNode, {NoiseInitParams} from './nodes/noise'
 
 const nodeTypes = { noiseNode: NoiseNode };
 
 
-const initialNodes = [
-   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-   { id: '3', position: { x: 0, y: 200 }, type: 'noiseNode', data: { label: '3' } },
+// https://reactflow.dev/api-reference/types/node
+const initialNodes:Node[] = [
+   // { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+   // { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+   { 
+      id: '3', 
+      position: { x: 0, y: 200 },
+      type: 'noiseNode',
+      dragHandle: '.draghandle',
+      data: { 
+         params: {...NoiseInitParams}
+      }
+   },
  ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 
 export default function Page() {
+
+   // 各ノードのパラメータはzustandで管理
+   // それをedgesとnodesが変更した時にコンパイルする
+
    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   
