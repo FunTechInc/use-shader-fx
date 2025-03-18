49f8cc7faa77d12b7e14837ca26849bc21f043b5
takuma-hmng8 <t.hashimoto@funtech.inc>
2025-03-18 11:10:42 +0900

    update

--- utils/useGUI.ts
+++ utils/useGUI.ts
@@ -1,33 +1,39 @@
-import { useCallback, useEffect, useState, RefObject } from "react";
+import { useCallback, useEffect, RefObject, useRef } from "react";
 import GUI from "lil-gui";
 
-export const useGUI = (setupGUI: (gui: GUI) => void, title?: string, container?: RefObject<HTMLDivElement>) => {
-   const [gui, setGUIState] = useState<GUI | null>(null);
+export const useGUI = (
+   setupGUI: (gui: GUI) => void,
+   title?: string,
+   container?: RefObject<HTMLDivElement>
+) => {
+   const guiRef = useRef<GUI | null>(null);
 
    useEffect(() => {
-      if (!gui) {
+      if (!guiRef.current) {
          const newGui = new GUI({
             closeFolders: true,
             width: 240,
             title,
             autoPlace: container?.current ? false : true,
-            container: container?.current || undefined,            
+            container: container?.current || undefined,
          });
-         setGUIState(newGui);
+         guiRef.current = newGui;
          setupGUI(newGui);
       }
+
       return () => {
-         if (gui) {
-            gui?.destroy();
-            setGUIState(null);
+         if (guiRef.current) {
+            guiRef.current.destroy();
+            guiRef.current = null;
          }
       };
-   }, [gui, setupGUI, title, container]);
+   }, [setupGUI, title, container]);
 
    const updateDisplays = useCallback(() => {
-      gui?.folders.forEach((folder) =>
+      guiRef.current?.folders.forEach((folder) =>
          folder.controllers.forEach((controller) => controller.updateDisplay())
       );
-   }, [gui]);
+   }, []);
+
    return updateDisplays;
 };
